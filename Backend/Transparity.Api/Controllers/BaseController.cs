2ef119dd7c634b6e3e41cfc065bc4c36e4ce31d7
tanstevn <tanstevenlester@gmail.com>
2026-03-03 03:40:13 +0800

    TPY-19: Add backend changes to trigger updated ci-build-backend.yml from previous commit

---
+++ Backend/Transparity.Api/Controllers/BaseController.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Transparity.Api.Controllers {
+    [ApiController, Produces("application/json")]
+    public class BaseController : ControllerBase { }
+}
