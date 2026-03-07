2080bbb6f3b51176d4a8e3610852190fa7e12468
tanstevn <tanstevenlester@gmail.com>
2026-03-08 02:54:06 +0800

    TPY-30: Add base unit test abstract classes that will be used for requests that without and with validators

---
+++ Backend/Transparity.Tests.Unit/Abstractions/BaseUnitTest.cs
@@ -0,0 +1,92 @@
+﻿using FluentAssertions;
+using FluentValidation.Results;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Transparity.Application.Abstractions;
+using Transparity.Data;
+
+namespace Transparity.Tests.Unit.Abstractions {
+    internal abstract class BaseUnitTest<TTestClass, TRequest, TResponse, TRequestHandler>
+        where TTestClass: BaseUnitTest<TTestClass, TRequest, TResponse, TRequestHandler>
+        where TRequest : IRequest<TResponse>
+        where TRequestHandler : class, IRequestHandler<TRequest, TResponse> {
+        protected Mock<ApplicationDbContext> _dbContext = default!;
+        protected Mock<TRequestHandler> _requestHandler = default!;
+        protected TRequest _request = default!;
+        protected TResponse _expected = default!;
+        private TResponse _result = default!;
+        private Exception _exception = default!;
+
+        protected BaseUnitTest() {
+            var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("Neon")
+                .Options;
+
+            _dbContext = new Mock<ApplicationDbContext>(dbContextOptions);
+        }
+
+        protected virtual void SetupMockRequestHandler(ValidationResult? expectedValidationResult = null) {
+            _requestHandler = new Mock<TRequestHandler>(_dbContext.Object);
+            _requestHandler.Setup(handler => handler.HandleAsync(_request))
+                .ReturnsAsync(_expected);
+        }
+
+        public TTestClass Arrange(Action<TRequest> arrangeRequest, Action<TResponse> arrangeExpected) {
+            _request = Activator.CreateInstance<TRequest>();
+            arrangeRequest(_request);
+
+            _expected = Activator.CreateInstance<TResponse>();
+            arrangeExpected(_expected);
+
+            SetupMockRequestHandler();
+            return (TTestClass)this;
+        }
+
+        public TTestClass Arrange(Action<TRequest> arrangeRequest, Action<ValidationResult> arrangeValidationResult) {
+            _request = Activator.CreateInstance<TRequest>();
+            arrangeRequest(_request);
+
+            var expectedValidationResult = new ValidationResult();
+            arrangeValidationResult(expectedValidationResult);
+
+            SetupMockRequestHandler(expectedValidationResult);
+            return (TTestClass)this;
+        }
+
+        public TTestClass Act() {
+            try {
+                _result = _requestHandler.Object
+                    .HandleAsync(_request)
+                    .GetAwaiter()
+                    .GetResult();
+            }
+            catch (Exception ex) {
+                _exception = ex;
+            }
+
+            return (TTestClass)this;
+        }
+
+        public void Assert(Action<TResponse>? assertion = null) {
+            _result.Should()
+                .Be(_expected);
+
+            if (assertion is not null) {
+                assertion(_result);
+            }
+        }
+
+        public void AssertThrows<TException>(Action<TException>? assertion = null)
+            where TException : Exception {
+            _exception.Should()
+                .NotBeNull();
+
+            _exception.Should()
+                .BeOfType<TException>();
+
+            if (assertion is not null) {
+                assertion((TException)_exception);
+            }
+        }
+    }
+}
---
+++ Backend/Transparity.Tests.Unit/Abstractions/BaseUnitTestWithValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Moq;
+using Transparity.Application.Abstractions;
+
+namespace Transparity.Tests.Unit.Abstractions {
+    internal class BaseUnitTestWithValidator<TTestClass, TRequest, TResponse, TRequestHandler, TRequestValidator> : BaseUnitTest<TTestClass, TRequest, TResponse, TRequestHandler>
+        where TTestClass : BaseUnitTestWithValidator<TTestClass, TRequest, TResponse, TRequestHandler, TRequestValidator>
+        where TRequest : IRequest<TResponse>
+        where TRequestHandler : class, IRequestHandler<TRequest, TResponse>
+        where TRequestValidator : class, IValidator<TRequest> {
+        private Mock<TRequestValidator> _validator = default!;
+
+        protected override void SetupMockRequestHandler(ValidationResult? expectedValidationResult = null) {
+            _validator = new Mock<TRequestValidator>();
+            _validator.Setup(validator => validator.ValidateAsync(_request))
+                .ReturnsAsync(expectedValidationResult ?? new ValidationResult());
+
+            _requestHandler = new Mock<TRequestHandler>(_dbContext.Object, _validator.Object);
+
+            var requestHandlerSetup = _requestHandler
+                .Setup(handler => handler.HandleAsync(_request));
+
+            if (expectedValidationResult is not null) {
+                requestHandlerSetup.ThrowsAsync(new ValidationException(expectedValidationResult.Errors));
+            }
+            else {
+                requestHandlerSetup.ReturnsAsync(_expected);
+            }
+        }
+    }
+}
