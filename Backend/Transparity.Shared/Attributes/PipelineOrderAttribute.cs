65aab56cd84277fab98392c97777388cfb4e2061
tanstevn <tanstevenlester@gmail.com>
2026-03-08 04:56:53 +0800

    TPY-30: Update with appsettings files

--- Backend/Transparity.Shared/Attributes/PipelineOrderAttribute.cs
+++ Backend/Transparity.Shared/Attributes/PipelineOrderAttribute.cs
@@ -1,7 +1,7 @@
 ﻿namespace Transparity.Shared.Attributes {
     [AttributeUsage(AttributeTargets.Class)]
     public class PipelineOrderAttribute : Attribute {
-        public short Order { get; private set; }
+        public short Order { get; }
 
         public PipelineOrderAttribute(short order) {
             Order = order;
